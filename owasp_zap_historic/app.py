545c454f50075787fb729caaadd461aa37915567
neil.howell <neil.howell@accruent.com>
2020-05-21 08:43:30 -0500

    QE-1879 work on app.py to get to pylint standards

--- owasp_zap_historic/app.py
+++ owasp_zap_historic/app.py
@@ -1,9 +1,9 @@
+"""Main module for OWASP ZAP Historic application."""
+import random
+import string
 from flask import Flask, render_template, request, redirect, url_for, session
 from flask_mysqldb import MySQL, MySQLdb
 import bcrypt
-import random
-import string
-import config
 from .args import parse_options
 
 app = Flask(__name__, template_folder='templates')
@@ -12,6 +12,7 @@ mysql = MySQL(app)
 
 @app.route('/', methods=['GET'])
 def index():
+    """render index.html page"""
     cursor = mysql.connection.cursor()
     use_db(cursor, "owaspzaphistoric")
     cursor.execute("select * from TB_PROJECT ORDER BY Project_Name ASC;")
@@ -21,16 +22,19 @@ def index():
 
 @app.route('/redirect')
 def redirect_url():
+    """redirect page"""
     return render_template('redirect.html')
 
 
 @app.route('/<db>/deldbconf', methods=['GET'])
 def delete_db_conf(db):
+    """Confirm deletion of a project page"""
     return render_template('deldbconf.html', db_name=db)
 
 
 @app.route('/<db>/delete', methods=['GET'])
 def delete_db(db):
+    """Actual deletion of project"""
     cursor = mysql.connection.cursor()
     cursor.execute("DROP DATABASE %s;" % db)
     # use_db(cursor, "robothistoric")
@@ -41,6 +45,7 @@ def delete_db(db):
 
 @app.route('/login', methods=["GET", "POST"])
 def login():
+    """Login page"""
     if request.method == 'POST':
         email = request.form['email']
         password = request.form['password'].encode('utf-8')
@@ -52,7 +57,8 @@ def login():
         curl.close()
 
         if len(user) > 0:
-            if bcrypt.hashpw(password, user["password"].encode('utf-8')) == user["password"].encode('utf-8'):
+            if bcrypt.hashpw(password, user["password"].encode('utf-8')) == \
+                    user["password"].encode('utf-8'):
                 session['name'] = user['name']
                 session['email'] = user['email']
                 return redirect(url_for('index'))
@@ -66,12 +72,14 @@ def login():
 
 @app.route('/logout', methods=["GET", "POST"])
 def logout():
+    """Logout page"""
     session.clear()
     return redirect(url_for('index'))
 
 
 @app.route('/register', methods=["GET", "POST"])
 def register():
+    """Register a new user page"""
     if request.method == 'GET':
         return render_template("register.html")
     else:
@@ -92,6 +100,7 @@ def register():
 
 @app.route('/newdb', methods=['GET', 'POST'])
 def add_db():
+    """Add a new project page"""
     if request.method == "POST":
         db_name = request.form['dbname']
         db_desc = request.form['dbdesc']
@@ -118,8 +127,8 @@ def add_db():
                 "Create table TB_ALERTS ( Alert_Id INT NOT NULL auto_increment primary key, "
                 "Execution_Id INT, Alert_Level TEXT, Alert_Type TEXT, URLS_Affected INT);")
             mysql.connection.commit()
-        except Exception as e:
-            print(str(e))
+        except Exception as exc:
+            print(str(exc))
 
         finally:
             return redirect(url_for('index'))
@@ -129,6 +138,7 @@ def add_db():
 
 @app.route('/<db>/dashboard', methods=['GET'])
 def dashboard(db):
+    """Project dashboard page"""
     cursor = mysql.connection.cursor()
     use_db(cursor, db)
 
@@ -234,193 +244,13 @@ def dashboard(db):
         return redirect(url_for('redirect_url'))
 
 
-@app.route('/<db>/ehistoric', methods=['GET'])
-def ehistoric(db):
-    cursor = mysql.connection.cursor()
-    use_db(cursor, db)
-    cursor.execute("SELECT * from TB_EXECUTION order by Execution_Id desc LIMIT 500;")
-    data = cursor.fetchall()
-    return render_template('ehistoric.html', data=data, db_name=db)
-
-
-@app.route('/<db>/deleconf/<eid>', methods=['GET'])
-def delete_eid_conf(db, eid):
-    return render_template('deleconf.html', db_name=db, eid=eid)
-
-
-@app.route('/<db>/edelete/<eid>', methods=['GET'])
-def delete_eid(db, eid):
-    cursor = mysql.connection.cursor()
-    use_db(cursor, db)
-    # remove execution from tables: execution, suite, test
-    cursor.execute("DELETE FROM TB_EXECUTION WHERE Execution_Id='%s';" % eid)
-    cursor.execute("DELETE FROM TB_SUITE WHERE Execution_Id='%s';" % eid)
-    cursor.execute("DELETE FROM TB_TEST WHERE Execution_Id='%s';" % eid)
-    # get latest execution info
-    cursor.execute("SELECT Execution_Pass, Execution_Total from TB_EXECUTION ORDER BY Execution_Id DESC LIMIT 1;")
-    data = cursor.fetchall()
-    # get no. of executions
-    cursor.execute("SELECT COUNT(*) from TB_EXECUTION;")
-    exe_data = cursor.fetchall()
-
-    try:
-        if data[0][0] > 0:
-            recent_pass_perf = float("{0:.2f}".format((data[0][0] / data[0][1] * 100)))
-        else:
-            recent_pass_perf = 0
-    except:
-        recent_pass_perf = 0
-
-    # update robothistoric project
-    cursor.execute(
-        "UPDATE robothistoric.TB_PROJECT SET Total_Executions=%s, Last_Updated=now(), Recent_Pass_Perc=%s WHERE Project_Name='%s';" % (
-        int(exe_data[0][0]), recent_pass_perf, db))
-    # commit changes
-    mysql.connection.commit()
-    return redirect(url_for('ehistoric', db=db))
-
-
-@app.route('/<db>/tmetrics', methods=['GET', 'POST'])
-def tmetrics(db):
-    cursor = mysql.connection.cursor()
-    use_db(cursor, db)
-    if request.method == "POST":
-        textField = request.form['textField']
-        rowField = request.form['rowField']
-        cursor.execute("Update TB_TEST SET Test_Comment='%s' WHERE Test_Id=%s;" % (str(textField), str(rowField)))
-        mysql.connection.commit()
-
-    # Get last row execution ID
-    cursor.execute("SELECT Execution_Id from TB_EXECUTION order by Execution_Id desc LIMIT 1;")
-    data = cursor.fetchone()
-    # Get testcase results of execution id (typically last executed)
-    cursor.execute("SELECT * from TB_TEST WHERE Execution_Id=%s;" % data)
-    data = cursor.fetchall()
-    return render_template('tmetrics.html', data=data, db_name=db)
-
-
-@app.route('/<db>/metrics/<eid>', methods=['GET'])
-def metrics(db, eid):
-    cursor = mysql.connection.cursor()
-    use_db(cursor, db)
-    # Get testcase results of execution id
-    cursor.execute("SELECT * from TB_TEST WHERE Execution_Id=%s;" % eid)
-    test_data = cursor.fetchall()
-    # get suite results of execution id
-    cursor.execute("SELECT * from TB_SUITE WHERE Execution_Id=%s;" % eid)
-    suite_data = cursor.fetchall()
-    # get project image
-    cursor.execute("SELECT Project_Image from robothistoric.TB_PROJECT WHERE Project_Name='%s';" % db)
-    project_image = cursor.fetchall()
-    # get execution info
-    cursor.execute("SELECT * from TB_EXECUTION WHERE Execution_Id=%s;" % eid)
-    exe_data = cursor.fetchall()
-    return render_template('metrics.html', suite_data=suite_data, test_data=test_data,
-                           project_image=project_image[0][0], exe_data=exe_data)
-
-
-@app.route('/<db>/tmetrics/<eid>', methods=['GET', 'POST'])
-def eid_tmetrics(db, eid):
-    cursor = mysql.connection.cursor()
-    use_db(cursor, db)
-    if request.method == "POST":
-        textField = request.form['textField']
-        rowField = request.form['rowField']
-        cursor.execute("Update TB_TEST SET Test_Comment='%s' WHERE Test_Id=%s;" % (str(textField), str(rowField)))
-        mysql.connection.commit()
-
-    # Get testcase results of execution id (typically last executed)
-    cursor.execute("SELECT * from TB_TEST WHERE Execution_Id=%s;" % eid)
-    data = cursor.fetchall()
-    return render_template('eidtmetrics.html', data=data, db_name=db)
-
-
-@app.route('/<db>/failures/<eid>', methods=['GET', 'POST'])
-def eid_failures(db, eid):
-    cursor = mysql.connection.cursor()
-    use_db(cursor, db)
-    if request.method == "POST":
-        textField = request.form['textField']
-        rowField = request.form['rowField']
-        cursor.execute("Update TB_TEST SET Test_Comment='%s' WHERE Test_Id=%s;" % (str(textField), str(rowField)))
-        mysql.connection.commit()
-
-    # Get testcase results of execution id (typically last executed)
-    cursor.execute("SELECT * from TB_TEST WHERE Execution_Id=%s and Test_Status='FAIL';" % eid)
-    data = cursor.fetchall()
-    return render_template('failures.html', data=data, db_name=db)
-
-
-@app.route('/<db>/search', methods=['GET', 'POST'])
-def search(db):
-    if request.method == "POST":
-        search = request.form['search']
-        cursor = mysql.connection.cursor()
-        use_db(cursor, db)
-        cursor.execute(
-            "SELECT * from TB_TEST WHERE Test_Name LIKE '%{name}%' OR Test_Status LIKE '%{name}%' OR Execution_Id LIKE '%{name}%' ORDER BY Execution_Id DESC LIMIT 10000;".format(
-                name=search))
-        data = cursor.fetchall()
-        return render_template('search.html', data=data, db_name=db)
-    else:
-        return render_template('search.html', db_name=db)
-
-
-@app.route('/<db>/flaky', methods=['GET'])
-def flaky(db):
-    cursor = mysql.connection.cursor()
-    use_db(cursor, db)
-    cursor.execute(
-        "SELECT Execution_Id from ( SELECT Execution_Id from TB_EXECUTION ORDER BY Execution_Id DESC LIMIT 5 ) as tmp ORDER BY Execution_Id ASC LIMIT 1;")
-    last_five = cursor.fetchall()
-    cursor.execute("SELECT COUNT(Execution_Id) from TB_EXECUTION;")
-    lastID = cursor.fetchall()
-    sql_query = "SELECT Execution_Id, Test_Name, Test_Status from TB_TEST WHERE Execution_Id >= %s ORDER BY Execution_Id DESC;" % (
-        str(last_five[0][0]))
-    one = int(lastID[0][0])
-    two = int(lastID[0][0]) - 1
-    three = int(lastID[0][0]) - 2
-    four = int(lastID[0][0]) - 3
-    five = int(lastID[0][0]) - 4
-    cursor.execute(sql_query)
-    data = cursor.fetchall()
-    # print("==== Before Sorted Data ===")
-    # print(data)
-    sorted_data = sort_tests(data)
-    # print("==== After Sorted Data ===")
-    # print(sorted_data)
-    return render_template('flaky.html', data=sorted_data, db_name=db, build1=one, build2=two, build3=three,
-                           build4=four, build5=five)
-
-
-@app.route('/<db>/compare', methods=['GET', 'POST'])
-def compare(db):
-    if request.method == "POST":
-        eid_one = request.form['eid_one']
-        eid_two = request.form['eid_two']
-        cursor = mysql.connection.cursor()
-        use_db(cursor, db)
-        # fetch first eid tets results
-        cursor.execute(
-            "SELECT Execution_Id, Test_Name, Test_Status, Test_Time, Test_Error from TB_TEST WHERE Execution_Id=%s;" % eid_one)
-        first_data = cursor.fetchall()
-        # fetch second eid test results
-        cursor.execute(
-            "SELECT Execution_Id, Test_Name, Test_Status, Test_Time, Test_Error from TB_TEST WHERE Execution_Id=%s;" % eid_two)
-        second_data = cursor.fetchall()
-        # combine both tuples
-        data = first_data + second_data
-        sorted_data = sort_tests(data)
-        return render_template('compare.html', data=sorted_data, db_name=db, fb=eid_one, sb=eid_two)
-    else:
-        return render_template('compare.html', db_name=db)
-
-
 def use_db(cursor, db_name):
+    """method to switch db"""
     cursor.execute("USE %s;" % db_name)
 
 
 def sort_tests(data_list):
+    """Test sorting method."""
     out = {}
     for elem in data_list:
         try:
@@ -431,6 +261,7 @@ def sort_tests(data_list):
 
 
 def main():
+    """Main application method"""
     args = parse_options()
 
     app.config['MYSQL_HOST'] = args.sqlhost
